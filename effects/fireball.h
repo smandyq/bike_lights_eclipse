0ba7df6d90240f53254699999719bdd91175571f
Smandy <internetofawesome@samsurl.com>
2016-01-24 21:11:23 -0800

    fireballs back to mid effect

--- effects/fireball.h
+++ effects/fireball.h
@@ -11,11 +11,11 @@
 #include "../bike_lights.h"
 
 #define FIREBALL_MAX_FREQ 50  //max amount of time to wait to fire a new fireball
-#define FIREBALL_SPEED_DIVISOR_MIN 2 //maximum power of two to make the speed divisor
+#define FIREBALL_SPEED_DIVISOR_MIN 3 //maximum power of two to make the speed divisor
 #define FIREBALL_SPEED_DIVISOR_MAX 20 //maximum power of two to make the speed divisor
 #define FIREBALL_MAX_WAIT 63
 #define FIREBALL_MAX_LUT_INC 6  //maximum amount of shortening of the tail of the fireball
-#define FIREBALL_MIN_LUT_INC 2  //minimum amount of shortening of the tail of the fireball
+#define FIREBALL_MIN_LUT_INC 3  //minimum amount of shortening of the tail of the fireball
 #define FIREBALL_MAX_LIFETIME 3000
 #define FIREBALL_MIN_LIFETIME 2000
 
--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ PololuLedStrip<9> middle_strip;
 PololuLedStrip<10> front_strip;
 
 //{{0x03, 0x4f, 0x20}, {0xd4, 0x24, 0x26}};
-rgb_color christmas[] = {{0x03, 0x8f, 0x20}, {0xff, 0x0, 0x0}};
+//rgb_color christmas[] = {{0x03, 0x8f, 0x20}, {0xff, 0x0, 0x0}};
 rgb_color halogen = {255, 197, 143};
 
 star_t front_stars[FRONT_LED_COUNT];
 star_collection_t front_star_coll = {&halogen, 1, front_stars, FRONT_LED_COUNT, 50};
 
-star_t mid_stars[MIDDLE_LED_COUNT];
-star_collection_t mid_star_coll = {(rgb_color*)christmas, 2, mid_stars, MIDDLE_LED_COUNT, 5, 3};
+//star_t mid_stars[MIDDLE_LED_COUNT];
+//star_collection_t mid_star_coll = {(rgb_color*)christmas, 2, mid_stars, MIDDLE_LED_COUNT, 5, 3};
 
 star_t rear_stars[REAR_LED_COUNT];
 star_collection_t rear_star_coll = {(rgb_color*)&rainbow[RED], 1, rear_stars, REAR_LED_COUNT, 10, 2};
@@ -43,8 +43,8 @@ void setup()
 	init_stars(&front_star_coll, front_colors);
 //	init_cop();
 //	update_blank();
-//	init_fireballs(fireballs, FIREBALL_COUNT, middle_colors, middle_colors+43);
-	init_stars(&mid_star_coll, middle_colors);
+	init_fireballs(fireballs, FIREBALL_COUNT, middle_colors, middle_colors+43);
+//	init_stars(&mid_star_coll, middle_colors);
 	init_stars(&rear_star_coll, rear_colors);
 }
 
@@ -63,19 +63,19 @@ void loop()  //once per "frame" of data
 //	update_stars(138,150);
 	update_stars(&front_star_coll, 0, 4);  //dead pixel situation
 	update_stars(&front_star_coll, 5, FRONT_LED_COUNT);
-	update_stars(&mid_star_coll, 0, MIDDLE_LED_COUNT);
+//	update_stars(&mid_star_coll, 0, MIDDLE_LED_COUNT);
 	update_stars(&rear_star_coll, 0, REAR_LED_COUNT);
 //	update_cop(125, 150);
 //	timer = micros();
-//	update_fireballs(fireballs, FIREBALL_COUNT);
+	update_fireballs(fireballs, FIREBALL_COUNT);
 //	timer = micros()-timer;
 //	Serial.print(timer);
 //	Serial.print("\r\n");
 	// Write the colors to the LED strip.
 	avg+=timer;
 
-	for(rgb_color* c=middle_colors; c<middle_colors+MIDDLE_LED_COUNT; c++)
-		apply_value(c, c, 255);
+//	for(rgb_color* c=middle_colors; c<middle_colors+MIDDLE_LED_COUNT; c++)
+//		apply_value(c, c, 255);
 	middle_strip.write(middle_colors, MIDDLE_LED_COUNT);
 	front_strip.write(front_colors, FRONT_LED_COUNT);
 	rear_strip.write(rear_colors, REAR_LED_COUNT);
